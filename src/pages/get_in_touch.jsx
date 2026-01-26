6d0d30fcb9c8731a007a436412248db9f30b900f
karthi3075 <karthijayakumar3007@gmail.com>
2026-01-26 22:03:11 +0530

    change

--- src/pages/get_in_touch.jsx
+++ src/pages/get_in_touch.jsx
@@ -1,10 +1,13 @@
-export default function Get_in_touch(){
+import Customer_support from "../components/customer_support";
+
+export default function Get_in_touch(){ 
     return(
         <div className="bg-white rounded m-2 border text-center p-2 font-bold">
             <h1 className="text-xl">Get in Touch</h1>
             <div className="flex justify-center mt-4">
-                <img src="/src/assets/photo (10).jpg" className="rounded-xl items-center" style={{height:"300px",width:"300px"}} alt="" />
+                <img src="/src/assets/image.webp" className="rounded-xl items-center" style={{height:"300px",width:"300px"}} alt="" />
             </div>
+            <Customer_support/>
         </div>
     )
 }   
