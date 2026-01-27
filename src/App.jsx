63c297a99e9b0c693a257193f0895f887ce35dae
Arsath-md <mdarsath.m2003@gmail.com>
2026-01-27 15:15:17 +0530

    changes on 27/01/26 3:15pm

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
 import Home from './pages/Home'
-import Service from './pages/Service'
-import Customers from './pages/Customers'
-
+import Services from './pages/Sevices'
+import Customer from './pages/customer'
+import News_letter from './pages/news_letter'
+import Get_in_touch from './pages/get_in_touch'
 
 function App() {
     return(
-        <>
-         <Home></Home>
-        <Service></Service>
-        <Customers/>
-        </>
+        <div className='bg-gray-200'>
+            <Home/>
+            <Services/>
+            <Customer/>
+            <News_letter/>
+            <Get_in_touch/>
+        </div>
     )
 }
 
